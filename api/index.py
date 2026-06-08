4a3043ed83a7c215c74e54082d48eb959be5d10f
shahzaib238 <161121604+shahzaib238@users.noreply.github.com>
2026-06-08 11:54:00 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,9 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-return templates.TemplateResponse("index.html", {"request": request})
-
-@app.get("/api/message")
-async def message():
-return {"message": "Marketplace is working"}
+@app.get("/")
+def home():
+    return {"status": "working"}
 
 handler = app

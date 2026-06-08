2712b561a8950015471bbdff4903c80385645947
shahzaib238 <161121604+shahzaib238@users.noreply.github.com>
2026-06-08 11:32:14 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,8 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def home():
-return {"message": "Marketplace working on Vercel"}
-
-
-handler = app
+async def home():
+    return {"message": "Marketplace is working"}
